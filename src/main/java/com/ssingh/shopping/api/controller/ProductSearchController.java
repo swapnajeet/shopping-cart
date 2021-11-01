f602e32d74eb3b91f9cdd5168e5c7c789121ce27
Singh Swapnajeet <swapnajeet.singh@telenor.no>
2021-11-01 13:43:25 +0100

    Upgraded hibernate search to fix free text search for products by their names. Implemented indexing of products by name at application start-up.

--- src/main/java/com/ssingh/shopping/api/controller/ProductSearchController.java
+++ src/main/java/com/ssingh/shopping/api/controller/ProductSearchController.java
@@ -3,14 +3,11 @@ package com.ssingh.shopping.api.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import com.ssingh.shopping.api.dto.PagingInfoDto;
 import com.ssingh.shopping.api.dto.ProductDto;
 import com.ssingh.shopping.api.service.ProductSearchService;
-import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -23,9 +20,9 @@ public class ProductSearchController {
         this.productSearchService = productSearchService;
     }
 
-    @GetMapping("/{textToSearch}")
-    public ResponseEntity<List<ProductDto>> updateProduct(@PathVariable String textToSearch, @Valid PagingInfoDto pagingInfoDto) {
-        List<ProductDto> matchedProducts = productSearchService.searchProducts(textToSearch, pagingInfoDto.getStartIndex(), pagingInfoDto.getPageSize());
+    @GetMapping("/")
+    public ResponseEntity<List<ProductDto>> searchProductByNameMatchingText(@RequestParam String textToSearch, @RequestParam Integer pageIndex, @RequestParam Integer pageSize) {
+        List<ProductDto> matchedProducts = productSearchService.getProductByNameMatchingText(textToSearch, pageIndex, pageSize);
         return ResponseEntity.status(HttpStatus.OK).body(matchedProducts);
     }
 }
--- src/main/java/com/ssingh/shopping/api/service/ProductSearchService.java
+++ src/main/java/com/ssingh/shopping/api/service/ProductSearchService.java
@@ -1,29 +1,36 @@
 package com.ssingh.shopping.api.service;
 
+import lombok.RequiredArgsConstructor;
+
+import org.apache.lucene.search.Query;
+import org.hibernate.search.jpa.FullTextEntityManager;
+import org.hibernate.search.jpa.FullTextQuery;
+import org.hibernate.search.jpa.Search;
+import org.hibernate.search.query.dsl.QueryBuilder;
 import org.springframework.stereotype.Service;
 import com.ssingh.shopping.api.assembler.ProductAssembler;
-import com.ssingh.shopping.api.dto.NewProductDto;
 import com.ssingh.shopping.api.dto.ProductDto;
-import com.ssingh.shopping.api.error.ProductNotFoundException;
 import com.ssingh.shopping.api.persistence.entity.Product;
-import com.ssingh.shopping.api.persistence.repository.ProductRepository;
-import com.ssingh.shopping.api.persistence.repository.ProductSearchRepository;
+import javax.persistence.EntityManager;
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
+@RequiredArgsConstructor
 @Service
 public class ProductSearchService {
 
-    private ProductSearchRepository searchRepository;
+    private final EntityManager entityManager;
 
-    public ProductSearchService(ProductSearchRepository searchRepository) {
-        this.searchRepository = searchRepository;
+    public List<ProductDto> getProductByNameMatchingText(String text, int pageIndex, int pageSize) {
+        FullTextEntityManager textEntityManager = Search.getFullTextEntityManager(entityManager);
+        QueryBuilder queryBuilder = textEntityManager.getSearchFactory().buildQueryBuilder().forEntity(Product.class).get();
+
+        Query query = queryBuilder.keyword().onField("name").matching(text).createQuery();
+
+        FullTextQuery fullTextQuery = textEntityManager.createFullTextQuery(query, Product.class);
+        fullTextQuery.setFirstResult(pageIndex);
+        fullTextQuery.setMaxResults(pageIndex * pageSize);
+        List<Product> resultList = (List<Product>) fullTextQuery.getResultList();
+        return resultList.stream().map(ProductAssembler::toProductDto).collect(Collectors.toList());
     }
-
-    public List<ProductDto> searchProducts(String searchText, int start, int count) {
-        List<Product> products = searchRepository.getProductNameByMatchingText(searchText, start, count);
-        return products.stream().map(ProductAssembler::toProductDto).collect(Collectors.toList());
-    }
-
 }
